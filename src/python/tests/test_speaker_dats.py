4c0362d8b5018e44195c4c4674e51b024a73c9ab
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-08-26 17:22:08 +0200

    [python] Improve DATS tests

--- src/python/tests/test_speaker_dats.py
+++ src/python/tests/test_speaker_dats.py
@@ -1,18 +1,21 @@
 import numpy as np
 
 from akustik.speaker.dats import (
+    find_f1_f2,
     max_impedance,
     read_dats_folder,
 )
 
 
-def test_max_sound_pressure():
+def test_find_f1_f2():
     folder = "src/python/tests/data/dats/RSS390HF-4"
     name, frd, zma = read_dats_folder(folder)
     assert name == "RSS390HF-4"
     assert frd.shape == (416, 3)
     assert zma.shape == (344, 3)
 
+    Re = 3.0
     Z: np.ndarray = zma["Impedance"].to_numpy()
     f: np.ndarray = zma["Frequency"].to_numpy()
     assert np.allclose(max_impedance(Z, f), (21.173, 19.585))
+    assert np.allclose(find_f1_f2(Re, Z, f), (13.454, 30.204))
